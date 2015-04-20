717d66aa0b016a328c1ae52e34600ef7372e9cd2
msoares <msoares@parks.com.br>
2015-04-20 17:20:02 -0300

    Rejected commit: Default

---
+++ app/src/main/java/mbs/carros/Login.java
@@ -0,0 +1,57 @@
+package mbs.carros;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void entrarOnClick(View view){
+        EditText usuario = (EditText) findViewById(R.id.usuario);
+        EditText password = (EditText) findViewById(R.id.password);
+
+        if(usuario.getText().toString().equals("teste") && password.getText().toString().equals("")){
+            Toast toast = Toast.makeText(this,"Erro na autenticação",Toast.LENGTH_SHORT);
+            toast.show();
+
+        }else{
+        startActivity(new Intent(this,DashBoard.class));
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
